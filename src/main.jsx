442afdd1e341f0fc350e6bda5d12c6604d7ddbcc
Charles Chao <bbchucks@gmail.com>
2026-01-12 20:38:16 -0500

    feat: add /backtest route

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import BacktestPage from './pages/BacktestPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
       <Route path="/" element={<App focusCoin="BTC" />} />
       <Route path="/eth" element={<App focusCoin="ETH" />} />
       <Route path="/sol" element={<App focusCoin="SOL" />} />
+      <Route path="/backtest" element={<BacktestPage />} />
     </Routes>
   </BrowserRouter>
 )
